51200c709d064e0d928a959beddcf2c0fb62f939
Zunda <mailme@jacobhaussman.online>
2026-06-08 14:58:03 -0400

    floating numbers

--- game/floating_text.gd
+++ game/floating_text.gd
@@ -2,8 +2,8 @@ extends Node2D
 class_name FlavorText
 
 var opacity = 1
-var initial_up_velocity = -30
-var x_speed = randi_range(-50, 50)
+var initial_up_velocity = -500
+var x_speed = randi_range(-200, 200)
 
 func _ready() -> void:
 	$Label.label_settings = $Label.label_settings.duplicate()
@@ -15,10 +15,10 @@ func set_flavor_text(value: String):
 	$Label.text = value
 
 func _process(delta: float) -> void:
-	initial_up_velocity += 100 * delta
+	initial_up_velocity += 2000 * delta
 	global_position.y += initial_up_velocity * delta
 	global_position.x += x_speed * delta
-	opacity -= 2 * delta
+	scale *= 1 - (delta * .5)
 
 	$Label.label_settings.font_color.a = opacity
 	$Label.label_settings.outline_color.a = opacity
