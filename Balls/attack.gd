5ee38ed19f4dcfa8bcbe6db04770f7ad6d8f28dd
Benji42 <cheezyanimations@gmail.com>
2026-06-09 17:35:26 -0400

    particle lines

--- Balls/attack.gd
+++ Balls/attack.gd
@@ -3,21 +3,26 @@ extends Node2D
 
 @export var start: Vector2
 @export var end: Vector2
+@export var particle_gravity = -9.8
+@export var speed = 500
 
 func _process(_delta: float) -> void:
 	if not Engine.is_editor_hint():
-		$GPUParticles2D.one_shot = true
-		$GPUParticles2D.emitting = false
+		pass
 
-	var R = abs(end.x - start.x)
-	var dy = start.y - end.y
+	$Path2D.curve.set_point_position(1, end)
 	
-	# 45 degree angle
-	var theta = atan(R / (dy + sqrt(R**2 + dy**2))) * (1./2.)
-	var v_zero = sqrt((98 * R**2) / (2 * (cos(theta)**2) * abs(dy - (R * tan(theta)))))
-
-	$GPUParticles2D.position = start
-	$GPUParticles2D.process_material.initial_velocity = Vector2(v_zero, v_zero)
-	$GPUParticles2D.process_material.direction = Vector3(cos(theta), -sin(theta), 0)
+	var x = end.x - start.x
+	var y = end.y - start.x
+	var g = particle_gravity
+	var a = -g * (x ** 2) / speed
+	var a_arrival = -g * (end.x ** 2) / speed
+	var tan_launch = -x + sqrt(x ** 2 + 4 * a * (y + a)) / (2 * a)
+	var launch = atan(tan_launch)
+	var tan_arrival = -end.x + sqrt(end.x ** 2 + 4 * a_arrival * (y + a_arrival)) / (2 * a_arrival)
+	var arrival = atan(tan_arrival)
+	
+	$Path2D.curve.set_point_out(0, Vector2i(cos(launch) * speed,sin(launch) * speed))
+	$Path2D.curve.set_point_in(1, Vector2i(cos(arrival) * speed,sin(arrival) * speed))
 
 	$Sprite2D.position = end
